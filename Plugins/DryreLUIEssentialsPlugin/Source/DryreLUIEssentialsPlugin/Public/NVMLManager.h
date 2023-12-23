5245efb2446852d8cfd669e236f26b1acffc9f7e
DryreL <gokdenizcetintr@gmail.com>
2023-12-23 05:52:23 +0300

    Comments added to NVMLManager

--- Plugins/DryreLUIEssentialsPlugin/Source/DryreLUIEssentialsPlugin/Public/NVMLManager.h
+++ Plugins/DryreLUIEssentialsPlugin/Source/DryreLUIEssentialsPlugin/Public/NVMLManager.h
@@ -1,10 +1,24 @@
-﻿#include <iostream>
+﻿// Copyright © 2024 - DryreL Design
+
+#include <iostream>
 #include "ThirdParty/NVML/include/nvml.h"
 
 // Function declaration
+
+// Initialize NVML
 int nvGPUInitializeNVML();
+
+// Shutdown NVML
 int nvGPUShutdownNVML();
+
+// Function to get GPU usage (in %)
 int nvGPUUtilizationNVML();
+
+// Function to get GPU Memory usage (in %)
 int nvMemoryUtilizationNVML();
+
+// Function to get GPU temperature (in Celsius)
 int nvGPUTemperatureNVML();
+
+// Function to get GPU Device Count
 int nvGPUDeviceCountNVML();
